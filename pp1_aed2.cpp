708744d2a70502f889efef5479c5b9ce90e574d3
Richardson Souza <richardson.allan.souza@gmail.com>
2016-10-20 16:58:17 -0400

    Update pp1_aed2.cpp

--- pp1_aed2.cpp
+++ pp1_aed2.cpp
@@ -5,37 +5,32 @@
 
 using namespace std;
 
-typedef int Vertex;
+typedef int T;
 const int BRANCO = 0;
 const int CINZA = 1;
 const int PRETO = 2;
 const int INFINITO = INT_MAX;
 int tempo;
 
-class Item {
+template <class T>
+class Planeta {
 private:
-    Vertex valor;
-    int cor, d, f;
-    Vertex predecessor;
-    vector<Vertex> vertices;
+    T valor;
+    int cor, d, f, menor_planeta_adjacente;
+    T predecessor;
+    vector<T> planeta_adjacentes;
+    vector<T> planeta_distancia;
 
 public:
     void print();
-    void printVertices();
-    Item() {};
-    Item(Vertex valor): valor(valor) { }
-    Item(Vertex valor, int cor, int d, int f, Vertex predecessor){
-    	this->valor = valor;
-    	this->cor = cor;
-    	this->d = d;
-    	this->f = f;
-    	this->predecessor = predecessor;
-    }
+    void printPlanetas();
+    Planeta() {};
+    Planeta(T valor): valor(valor) { }
 
-    Vertex getVertex() {
+    T getValor() {
         return valor;
     }
-    void setVertex(Vertex valor) {
+    void setValor(T valor) {
         this -> valor = valor;
     }
     int getCor(){
@@ -56,47 +51,187 @@ public:
     int getF(){
     	return f;
     }
-    Vertex getPredecessor(){
+    int getMenor() {
+    	return menor_planeta_adjacente;
+    }
+    void setMenor(int menor_planeta_adjacente) {
+    	this->menor_planeta_adjacente = menor_planeta_adjacente;
+    }
+    T getPredecessor(){
         return predecessor;
     }
-    void setPredecessor(Vertex predecessor){
+    void setPredecessor(T predecessor){
     	this->predecessor = predecessor;
     }
-    vector<Vertex> getVertices(){
-    	return vertices;
+    vector<T> getPlanetas(){
+    	return planeta_adjacentes;
     }
-    void setVertices(Vertex v){
-    	this->vertices.push_back(v);
+    void setPlanetas(T v){
+    	this->planeta_adjacentes.push_back(v);
+    }
+    void setDistancia(T v){
+    	this->planeta_distancia.push_back(v);
     }
 };
 
-void Item::print(){
-  cout << getVertex() << " ";
+template <class T>
+void Planeta<T>::print(){
+  cout << getValor() << " ";
 }
 
-void Item::printVertices(){
-  for(int i = 0; i < vertices.size(); i++){
-    cout << vertices[i] << " ";
+template <class T>
+void Planeta<T>::printPlanetas(){
+  for(int i = 0; i < planeta_adjacentes.size(); i++){
+    cout << planeta_adjacentes[i] << " ";
   }
 }
 
-class Grafo { // Não-direcionado
+template <class T>
+class Sistema {
 private:
-  	Item *adj;
-  	int n, m; // ordem e tamanho
+    T valor;
+    int cor, d;
+    T predecessor;
+    vector<T> sistema_adjacentes;
 
 public:
-  	Grafo(int); // construtor
-  	void initialize(int);
+    void print();
+    void printVertices();
+    Sistema() {};
+    Sistema(T valor): valor(valor) { }
+
+    T getValor() {
+        return valor;
+    }
+    void setValor(T valor) {
+        this -> valor = valor;
+    }
+    int getCor(){
+    	return cor;
+    }
+    void setCor(int cor){
+    	this->cor = cor;
+    }
+    void setD(int d){
+    	this->d = d;
+    }
+    int getD(){
+    	return d;
+    }
+    T getPredecessor(){
+        return predecessor;
+    }
+    void setPredecessor(T predecessor){
+    	this->predecessor = predecessor;
+    }
+    vector<T> getSistemas(){
+    	return sistema_adjacentes;
+    }
+    void setSistemas(T v){
+    	this->sistema_adjacentes.push_back(v);
+    }
+};
+
+template <class T>
+void Sistema<T>::print(){
+  cout << getValor() << " ";
+}
+
+template <class T>
+void Sistema<T>::printVertices(){
+  for(int i = 0; i < sistema_adjacentes.size(); i++){
+    cout << sistema_adjacentes[i] << " ";
+  }
+}
+
+template <class T>
+class Fila{
+private:
+	T *vertices;
+	int frente, tras;
+	int TAM;
+public:
+	Fila(int tam);
+	void enfileira(T);
+	T desenfileira();
+	void mostra();
+	bool vazia();
+};
+
+template <class T>
+Fila<T>::Fila(int tam){
+	TAM = tam;
+	vertices = new T[TAM];
+	frente = 0;
+	tras = frente;
+}
+
+template <class T>
+void Fila<T>::enfileira(T it){
+	if((tras + 1) % TAM == frente){
+		cout << "Fila Cheia!!" << endl;
+	}
+	else
+	{
+		vertices[tras] = it;
+		tras = (tras+1) % TAM;
+	}
+}
+
+template <class T>
+T Fila<T>::desenfileira(){
+	if(frente == tras){
+		cout << "Fila vazia!!" << endl;
+	}
+	else
+	{
+		T it;
+		it = vertices[frente];
+		frente = (frente + 1) % TAM;
+		return it;
+	}
+}
+
+template <class T>
+void Fila<T>::mostra(){
+	for(int i = frente; i < tras; i++){
+		vertices[i].print();
+	}
+	cout << endl;
+}
+
+template <class T>
+bool Fila<T>::vazia(){
+	if(frente == tras){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+template <class T>
+class Grafo {
+private:
+  	T *adj;
+  	int n, m;
+
+public:
+  	Grafo(int);
+  	void iniciar(int);
   	void preenche();
-  	void insertEdge(Vertex, Vertex);
+  	void inserirSistema(T, T);
+  	void inserirPlaneta(T, T, float);
   	void print();
   	void destroy();
-// métodos get/set para n, m e adj.
-	Item* getAdj(){
+  	void dfsVisita(T &);
+  	void dfs();
+  	void bfs(T &);
+
+	T* getAdj(){
 		return adj;
 	}
-	void setAdj(Item* adj){
+	void setAdj(T* adj){
 		this->adj = adj;
 	}
 	int getN(){
@@ -113,216 +248,127 @@ public:
 	}
 };
 
-Grafo::Grafo (int n) {
-  initialize(n);
+template <class T>
+Grafo<T>::Grafo (int n) {
+  iniciar(n);
   preenche();
 }
 
-void Grafo::initialize(int n) {
-  //if (this->n != 0){
+template <class T>
+void Grafo<T>::iniciar(int n) {
     this->n = n;
-    adj = new Item[n+1]; // Vetor usa
-    //inicializar os itens do vetor de 1 a n com os respectivos numeros de vertice
-  //}  // células de 1..n
+    adj = new T[n+1];
 }
 
-void Grafo::preenche(){
-  for(Vertex i = 1; i <= n; i++){
-	Item j(i);
+template <class T>
+void Grafo<T>::preenche(){
+  for(T i = 1; i <= n; i++){
+	T j(i);
 	adj[i] = j;
    }
 }
 
-void Grafo::insertEdge(Vertex u, Vertex v) {
-  //Item *x = new Item(v); // chave = vértice
-  adj[u].setVertices(v); // Insere na lista // vetor de itens(adj) pega a lista(getVertices) e adiciona
-  //x = new Item (u);
-  adj[v].setVertices(u); // Insere na lista
-  m++;
+template <class T>
+void Grafo<T>::inserirSistema(T u, T v) {
+	adj[u].setSistemas(v);
+	adj[v].setSistemas(u);
+	m++;
 }
 
-void Grafo::print() {
-  for (int i = 1; i <= n; i++) {
-    cout << "v[" << adj[i].getVertex() << "] = ";
-    adj[i].printVertices();
-    cout << endl;
+template <class T>
+void Grafo<T>::inserirPlaneta(T u, T v, float f) {
+
+	adj[u].setPlanetas(v);
+	adj[v].setPlanetas(u);
+
+	adj[u].setDistancia(f);
+	adj[v].setDistancia(f);
+
+	m++;
+}
+
+template <class T>
+void Grafo<T>::print() {
+	for (int i = 1; i <= n; i++) {
+		cout << "v[" << adj[i].getValor() << "] = ";
+		adj[i].printVertices();
+		cout << endl;
   }
 }
 
-/*void Grafo::destroy() {
-  for (int i = 0; i <= n; i++) {
-    adj[i].destroy(); // destroi lista
-  }
-  delete( adj );
-  n = m = 0;
-}*/
-
-//busca em profundidade
-void dfsVisita(Grafo &g, Item &u){
+template <class T>
+void Grafo<T>::dfsVisita(T &u){
 	tempo = tempo+1;
 	u.setD(tempo);
 	u.setCor(CINZA);
-	//cout << u.getVertex() << " ";
-	Vertex vertice_adj;
+	T vertice_adj;
 	for(int i = 0; i < u.getVertices().size(); i++){
 		vertice_adj = u.getVertices()[i];
-		if(g.getAdj()[vertice_adj].getCor() == BRANCO){
-			g.getAdj()[vertice_adj].setPredecessor(u.getVertex());
-			dfsVisita(g, g.getAdj()[vertice_adj]);
+		if(getAdj()[vertice_adj].getCor() == BRANCO){
+			getAdj()[vertice_adj].setPredecessor(u.getValor());
+			dfsVisita(getAdj()[vertice_adj]);
 		}
 	}
 	u.setCor(PRETO);
 	tempo = tempo+1;
 	u.setF(tempo);
-	cout << u.getVertex() << " ";
+	cout << u.getValor() << " ";
 }
 
-void dfs(Grafo &g){
-	for(int i = 1; i <= g.getN(); i++){
-		g.getAdj()[i].setCor(BRANCO);
-		g.getAdj()[i].setPredecessor(0);
+template <class T>
+void Grafo<T>::dfs(){
+	for(int i = 1; i <= getN(); i++){
+		getAdj()[i].setCor(BRANCO);
+		getAdj()[i].setPredecessor(0);
 	}
 
 	tempo = 0;
-	for(int j = 1; j <= g.getN(); j++){
-		if(g.getAdj()[j].getCor() == BRANCO){
-			dfsVisita(g, g.getAdj()[j]);
+	for(int j = 1; j <= getN(); j++){
+		if(getAdj()[j].getCor() == BRANCO){
+			dfsVisita(getAdj()[j]);
 		}
 	}
 }
 
-class Fila{
-private:
-	Item *vertices; //vetor
-	int frente, tras;
-	int TAM;
-public:
-	Fila(int tam);
-	void enfileira(Item);
-	Item desenfileira();
-	void mostra();
-	bool vazia();
-};
-Fila::Fila(int tam){
-	TAM = tam;
-	vertices = new Item[TAM];
-	frente = 0;
-	tras = frente;
-}
-void Fila::enfileira (Item it){
-	if((tras + 1) % TAM == frente){
-		cout << "Fila Cheia!!" << endl;
+template <class T>
+void Grafo<T>::bfs(T &s){
+	for(int i = 1; i <= getN(); i++){
+		getAdj()[i].setCor(BRANCO);
+		getAdj()[i].setD(INFINITO);
+		getAdj()[i].setPredecessor(0);
 	}
-	else
-	{
-		vertices[tras] = it;
-		tras = (tras+1) % TAM;
-	}
-}
-Item Fila::desenfileira(){
-	if(frente == tras){
-		cout << "Fila vazia!!" << endl;
-		//return NULL;
-	}
-	else
-	{
-		Item it;
-		it = vertices[frente];
-		frente = (frente + 1) % TAM;
-		return it;
-	}
-}
-void Fila::mostra(){
-	for(int i = frente; i < tras; i++){
-		vertices[i].print();
-	}
-	cout << endl;
-}
-bool Fila::vazia(){
-	if(frente == tras){
-		return true;
-	}
-	else{
-		return false;
-	}
-}
 
-//busca em largura
-void bfs(Grafo &g, Item &s){
-	for(int i = 1; i <= g.getN(); i++){
-		g.getAdj()[i].setCor(BRANCO);
-		g.getAdj()[i].setD(INFINITO);
-		g.getAdj()[i].setPredecessor(0);
-	}
 	s.setCor(CINZA);
 	s.setD(0);
 	s.setPredecessor(0);
 
-	Fila fila(g.getN());
-	fila.enfileira(s);
-	Item u;
-	Vertex posicao;
-	while(!fila.vazia()){
+	Fila<T> fila;
+	fila = new Fila(getN());
+
+	T u;
+	T posicao;
+	while(!fila){
 		u = fila.desenfileira();
 		for(int i = 0; i < u.getVertices().size(); i++){
 			posicao = u.getVertices()[i];
-			if(g.getAdj()[posicao].getCor() == BRANCO){
-				g.getAdj()[posicao].setCor(CINZA);
-				//cout << g.getAdj()[posicao].getVertex() << " ";
-				g.getAdj()[posicao].setD(u.getD()+1);
-				g.getAdj()[posicao].setPredecessor(u.getVertex());
-				fila.enfileira(g.getAdj()[posicao]);
+			if(getAdj()[posicao].getCor() == BRANCO){
+				getAdj()[posicao].setCor(CINZA);
+				getAdj()[posicao].setD(u.getD()+1);
+				getAdj()[posicao].setPredecessor(u.getValor());
+				fila.enfileira(getAdj()[posicao]);
 			}
 		}
 		u.setCor(PRETO);
-		cout << u.getVertex() << " ";
+		cout << u.getValor() << " ";
 	}
 }
 
-// Função auxiliar
-void testaGrafo(Grafo &g) {
-  g.insertEdge(1, 2);
-  g.insertEdge(2, 3);
-  g.insertEdge(3, 4);
-  g.insertEdge(4, 5);
-  g.insertEdge(5, 1);
-  g.insertEdge(5, 2);
-  g.insertEdge(2, 4);
-  g.print();
-}
-
 int main(int argc, const char * argv[]) {
-  //int n;
-  //cout << "ordem: "; cin >> n;
-  Grafo g(5);
-  //cout << "-----grafo-----" << endl;
-  testaGrafo(g);
-  //dfs(g);
-  bfs(g, g.getAdj()[1]);
-  /*Fila f(5);
-  f.enfileira(g.getAdj()[1]);
-  f.enfileira(g.getAdj()[5]);
-  f.mostra();
-  Item it;
-  it = f.desenfileira();
-  it.print();
-  f.mostra();*/
-  //cout << g.getAdj()[1].getPredecessor() << endl;
-  cout << endl << "fim" << endl;
-  /*g.getAdj()[1].setCor(CINZA);
-  g.getAdj()[3].setCor(BRANCO);
-  g.getAdj()[5].setCor(PRETO);
-  cout << g.getAdj()[1].getCor() << endl;
-  cout << g.getAdj()[2].getCor() << endl;
-  cout << g.getAdj()[3].getCor() << endl;
-  cout << g.getAdj()[4].getCor() << endl;
-  cout << g.getAdj()[5].getCor() << endl;*/
-  //testaGrafo(g);
-  //cout << "-----fim-------" << endl;
 
-  //g.getAdj()[1].print();
-  //dfs(g);
-  //cout << g.getAdj()->getPrim()->getProx()->getItem()->getCor();
-  //cout << "-----fim-------";
-  return 0;
+
+
+
+
+
+	return 0;
 }
